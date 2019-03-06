21bae3cf2445b40def4a0f2585d74421d1baef31
Patrick Kim <pkim050@gmail.com>
2019-03-06 18:37:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
   # write code here
-  attr_accessor :equilateral, :isosceles, :scalene #equilateral = all same sides. isosceles = two of same side. scalene = all different sides.
+  attr_accessor :one, :two, :three, :equilateral, :isosceles, :scalene #equilateral = all same sides. isosceles = two of same side. scalene = all different sides.
   
   def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind
     if one > 0 && two && 0 && three > 0
       if (one + two) > three && (one + three) > two && (two + three) > one
         return :equilateral if one == two && one == three
